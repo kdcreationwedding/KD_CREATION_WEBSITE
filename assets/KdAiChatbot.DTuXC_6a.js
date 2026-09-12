50110b1c516a2af60ceb9612c07b497160e89409
kdcreationwedding <kdcreationwedding@gmail.com>
2026-09-12 13:26:10 +0530

    Updates

--- assets/KdAiChatbot.DTuXC_6a.js
+++ assets/KdAiChatbot.DTuXC_6a.js
@@ -1,4 +1,4 @@
-import{r as h,j as e,m as I,A as j}from"./vendor-framer.C8kokVM1.js";import{S as k,L as A}from"./index.381HiJha.js";import{S as E,a0 as D,a1 as _,X as W,a2 as P}from"./vendor-icons.DQUz8hs3.js";class N{static getInitialState(){const p={id:"init-1",sender:"ai",text:`Namaste & Warm Welcome! ✨
+import{r as h,j as e,m as I,A as j}from"./vendor-framer.C8kokVM1.js";import{S as k,L as A}from"./index.C_fiyw4R.js";import{S as E,a0 as D,a1 as _,X as W,a2 as P}from"./vendor-icons.DQUz8hs3.js";class N{static getInitialState(){const p={id:"init-1",sender:"ai",text:`Namaste & Warm Welcome! ✨
 
 I am **KD AI**, your Chief Wedding Film Consultant at **KD CREATION**.
 
